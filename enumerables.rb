061b50057514d7773f6c83937b87170d7c6f1bbe
SamanAtashi <milady00126@gmail.com>
2021-05-06 10:47:04 +0300

    Fix linters - 1

--- enumerables.rb
+++ enumerables.rb
@@ -1,21 +1,17 @@
-# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
-
-    def my_each
-        if block_given? 
-            i=0
-            while i<self.length
-                    if self.class == Array
-                        yield self[i]
-                    else
-                        yield(self.keys[i],self.values[i])
-                    end
-            i+=1
-            end
-        else 
-            puts "block was not given"
+  def my_each
+    if block_given?
+      i = 0
+      while i < length
+        if instance_of?(Array)
+          yield self[i]
+        else
+          yield(keys[i], values[i])
         end
+        i += 1
+      end
+    else
+      puts 'block was not given'
     end
-
+  end
 end
-# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
