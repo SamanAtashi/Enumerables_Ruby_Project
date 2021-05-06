dc88a31e07c53e9417fd93c0e05f62bf343de1b6
SamanAtashi <milady00126@gmail.com>
2021-05-06 10:52:49 +0300

    Add my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -14,4 +14,19 @@ module Enumerable
       puts 'block was not given'
     end
   end
+  def my_each_with_index
+    if block_given?
+      i = 0
+      while i < length
+        if instance_of?(Array)
+          yield(self[i],i)
+        else
+          yield(values[i],i)
+        end
+        i += 1
+      end
+    else
+      puts 'block was not given'
+    end
+  end
 end
